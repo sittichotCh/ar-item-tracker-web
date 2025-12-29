37948d7799facac7c23edfc50fc7cabbd478cf58
Sittichot C <sittichot.c@gmail.com>
2025-12-29 12:09:54 +0700

    add gp

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+    plugins: [react()], base: '/ar-item-tracker-web/',  // เปลี่ยนเป็นชื่อ repo ของจริง
 })
